67c7c5e9241021ccadbe661be86327c94d67c73b
slick-solutions <36201692+ASTR0NAUTJ0NES@users.noreply.github.com>
2018-10-08 12:49:02 -0400

    Initial commit

---
+++ MultTable.java
@@ -0,0 +1,19 @@
+/**
+ * A program to print a multiplication table
+ * 
+ * @author Andrew Peyton Albanese
+ * @version 2016-01-23
+ */
+
+ public class MultTable
+{
+   public static void main(String[] args)
+   {
+      System.out.println("  y | 1y | 2y | 3y | 4y | 5y ");
+      System.out.println("----|----|----|----|----|----");
+      System.out.println("  1 |  1 |  2 |  3 |  4 |  5 ");
+      System.out.println("  2 |  2 |  4 |  6 |  8 | 10 ");
+      System.out.println("  3 |  3 |  6 |  9 | 12 | 15 ");
+      System.out.println("  4 |  4 |  8 | 16 | 16 | 20 ");
+   }
+}
